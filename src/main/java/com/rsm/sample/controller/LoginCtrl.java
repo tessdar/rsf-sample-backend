c6467b9222452d27c0de9468d9ed456d344cf8cc
kimwonchul <kimwonchul@218.38.137.28>
2018-11-28 01:03:07 +0900

    Test Last

--- src/main/java/com/rsm/sample/controller/LoginCtrl.java
+++ src/main/java/com/rsm/sample/controller/LoginCtrl.java
@@ -56,29 +56,30 @@ public class LoginCtrl {
 
 	@PostMapping(path = "/send-push", consumes = "application/json")
 	@ResponseBody
-	public ResponseEntity<Map<String, Object>> sendPush() {
+	public ResponseEntity<Map<String, Object>> sendPush(@RequestBody PushInfoVo vo) {
 
 		result.clear();
-//		ObjectMapper mapper = new ObjectMapper();
-//
-//		String jsonInString = null;
+		ObjectMapper mapper = new ObjectMapper();
+
+		String jsonInString = null;
 
 		try {
-//			jsonInString = mapper.writeValueAsString(vo);
-//			System.out.println(jsonInString);
+			jsonInString = mapper.writeValueAsString(vo);
+			System.out.println(jsonInString);
 
 			HttpResponse<String> response = Unirest.post("https://fcm.googleapis.com/fcm/send")
 					.header("Content-Type", "application/json")
 					.header("Authorization",
 							"key=AAAAhp3z7Co:APA91bHzwMP0_cx0qTcFlAUD9kN7xtTufNk_IG5QV7P9_BIJh7n9Qbg3p5rVk5G0ECbHUbRzTiJxSDSiyNGdeUzMKX_Vo6C5OBguvjAMpctFRBjSBZnf17caTlC4jKRszcOyofMc9swW5ZUIEAbhqf2Cg5iKH479VA")
 					.header("cache-control", "no-cache")
-					.body("{ \"registration_ids\" : [ \"eYkYAcBUWDQ:APA91bFtN1dsRd3AcqJ_LC2iTZryF-XjLe-bX3hetUiYaSRDI8KulNotmPt9wmPub5OgBdn1ciNqi8nL8crFOiOeZXDquxSugn2jDeYhDBhlo524-SIaQ9gQ7NFlga5uKtoLvXv8MgQM\", \"fKd3ZCVxYIk:APA91bHuYu2EbMN3mZHh-_hJNDBESniVMJ-h4psk5FPPfNc876S2tecM6qLTG5rEzJGQX5Z67aI5Z7akB959GMDrHDx47kfh3wbi5StxHeDlTh0-DvdfNNweJY1umQKXHYaA_6OL2xLL\" ], \"notification\": { \"title\" : \"456789\", \"body\" : \"456789\" }, \"data\": { \"trimNo\" : \"234567\", \"model\" : \"890\", \"diverLoc\" : \"123\", \"destination\" : \"456\", \"swBzr\" : \"78\", \"rrack\" : \"90\" } }\n\n")
+					.body(jsonInString)
+//					.body("{ \"registration_ids\" : [ \"eYkYAcBUWDQ:APA91bFtN1dsRd3AcqJ_LC2iTZryF-XjLe-bX3hetUiYaSRDI8KulNotmPt9wmPub5OgBdn1ciNqi8nL8crFOiOeZXDquxSugn2jDeYhDBhlo524-SIaQ9gQ7NFlga5uKtoLvXv8MgQM\", \"fKd3ZCVxYIk:APA91bHuYu2EbMN3mZHh-_hJNDBESniVMJ-h4psk5FPPfNc876S2tecM6qLTG5rEzJGQX5Z67aI5Z7akB959GMDrHDx47kfh3wbi5StxHeDlTh0-DvdfNNweJY1umQKXHYaA_6OL2xLL\" ], \"notification\": { \"title\" : \"456789\", \"body\" : \"456789\" }, \"data\": { \"trimNo\" : \"234567\", \"model\" : \"890\", \"diverLoc\" : \"123\", \"destination\" : \"456\", \"swBzr\" : \"78\", \"rrack\" : \"90\" } }\n\n")
 					.asString();
 
 			result.put("status", response.getStatus());
 
-//		} catch (JsonProcessingException e) {
-//			logger.error(e.getMessage());
+		} catch (JsonProcessingException e) {
+			logger.error(e.getMessage());
 
 		} catch (UnirestException e) {
 			logger.error(e.getMessage());
